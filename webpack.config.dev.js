cedd9deaff075c37876fb5d30e631d6cadc0997a
sakenpolatov <119658646+sakenpolatov@users.noreply.github.com>
2024-01-16 12:16:12 +0600

    feat : weather-sounds

---
+++ webpack.config.dev.js
@@ -0,0 +1,21 @@
+const { merge } = require('webpack-merge')
+const commonConfig = require('./webpack.config.common')
+
+module.exports = merge(commonConfig, {
+	mode: 'development',
+	devtool: 'inline-source-map',
+	devServer: {
+		port: 3000,
+		hot: true,
+		open: true
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js)$/,
+				exclude: /node_modules/,
+				use: ['eslint-loader']
+			}
+		]
+	}
+})
